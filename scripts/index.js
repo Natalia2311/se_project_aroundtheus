5e7781c43f10b196de56fff3248fa47d88781ff0
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2023-09-11 20:44:46 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 const initialCards = [
-
-{
+  {
     name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-},
-{
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-},
-{
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
     name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-},
-{
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-},
-{
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
     name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-},
-{
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-},
-]
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
 console.log(initialCards);
 
 //Elements 
@@ -32,21 +31,62 @@ console.log(initialCards);
 const profileEditButton = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseModal = document.querySelector("#edit-modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
 
-console.log(profileEditButton);
-console.log(profileEditModal);
-console.log(profileCloseModal);
+  
+// Functions 
 
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
-});
-
-profileCloseModal.addEventListener("click", () => {
+function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+};
+
+function openPopup() {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    profileEditModal.classList.add("modal_opened");
+  
+};
+
+
+
+function getCardElement(cardData) {  
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector('.card__image');
+  const cardTitleEl = cardElement.querySelector('card__title');
+  cardImageEl.setAttribute('src', cardData.link);
+  cardImageEl.setAttribute('alt', cardData.name);
+  cardTitleEl.textContent = cardData.name;
+  return cardElement;
+}
+
+// Event Handlers
+
+function handleProfileEditSubmit(e) {
+e.preventDefault();
+profileTitle.textContent = profileTitleInput.value;
+profileDescription.textContent = profileDescriptionInput.value;
+closePopup();
+};
+
+
+
+// Event Listeners
+
+profileEditButton.addEventListener("click", openPopup);
+
+profileCloseModal.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  
+  cardListEl.append(cardElement);
+
 });
-
-
-
-
-
-
