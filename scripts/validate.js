5785236d0f8e2071c16525ceaada1d2d9003e0f4
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2023-11-11 13:43:00 -0500

    Update validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,21 +1,18 @@
 
 
-function showInputError(formEl, inputEl, { errorClass } ) {
+function showInputError(formEl, inputEl, {inputErrorClass, errorClass} ) {
     const errorMessageEl = formEl.querySelector('#${inputEl.id}-error'); 
-    const  inputErrorClass = options.inputErrorClass;
     inputEl.classList.add(inputErrorClass); 
     errorMessageEl.textContent = inputEl.validationMessage;
     errorMessageEl.classList.add(errorClass);
   }
- 
-  function hideInputError(formEl, inputEl) {
-    const errorMessageEl = formEl.querySelector('#${inputEl.id}-error'); 
-    const errorClass = options.errorClass;
-    const inputErrorClass = options.inputErrorClass;
-    inputEl.classList.remove(inputErrorClass); 
-    errorMessageEl.textContent = "";
-    errorMessageEl.classList.remove(errorClass);
-  }
+   
+  function hideInputError(formEl, inputEl, {inputErrorClass, errorClass} ) {
+      const errorMessageEl = formEl.querySelector('#${inputEl.id}-error'); 
+      inputEl.classList.remove(inputErrorClass); 
+      errorMessageEl.textContent = "";
+      errorMessageEl.classList.remove(errorClass);
+    }
 
   function checkInputValidity(formEl, inputEl) {
     if(!inputEl.validity.valid) {
@@ -55,7 +52,7 @@ function toggleButtonState (inputEls, submitButton, { inactiveButtonClass }) {
 function setEventListeners(formEl, options) {
     const { inputSelector } = options;
     const inputEls = [...formEl.querySelectorAll(inputSelector)];
-    const submitButton = formEl.querySelector(submitButtonSelector);
+    const submitButton = formEl.querySelector(options.submitButtonSelector);
     toggleButtonState(inputEls, submitButton, options);
     inputEls.forEach((inputEl) => {
         inputEl.addEventListener("input", (e) => {
