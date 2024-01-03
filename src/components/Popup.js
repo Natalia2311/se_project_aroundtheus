e9ac0693b9173a65cf3c1afd9113995ef9919b53
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2024-01-02 19:27:15 -0500

    Add files via upload

---
+++ src/components/Popup.js
@@ -0,0 +1,31 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(popupSelector);
+    }
+    open() {
+        this._popupElement.classList.add("modal_opened");
+        document.addEventListener("keydown", this._handleEscape);
+    }
+
+    close() {
+        this._popupElement.classList.remove("modal_opened");
+        document.removeEventListener("keydown", this._handleEscape); 
+    }
+
+    _handleEscape = (evt) => {
+        if (evt.key === "Escape") {
+            this.close();
+          } 
+    }
+
+    setEventListeners() {
+        this._popupElement.addEventListener("click", (evt) => { 
+        if (
+         evt.target === this._popupElement ||
+         evt.target.classList.contains("modal__close")
+        ) {
+            this.close();
+        }
+          });
+    }
+}
