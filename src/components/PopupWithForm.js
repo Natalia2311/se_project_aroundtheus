1fa7ceee69e8f8fcd620953013369a11e13a01b7
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2024-01-15 13:30:44 -0500

    Add files via upload

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,43 @@
+import Popup from "./Popup";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = this._popupForm.querySelectorAll(".modal__form-input");
+    this._submitButton = this._popupForm.querySelector(".modal__button");
+    
+  }
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
+
+
+
+  setLoading(isLoading) {
+    if (isLoading) {
+        this._submitButton.textContent = "Saving ...";
+    } else {
+        this._submitButton.textContent = this._submitButton;
+    }       
+}
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+  setEventListeners() {
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+    super.setEventListeners();
+  }
+}
+
+export default PopupWithForm;
