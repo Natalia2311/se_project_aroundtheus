4ab98a02b8409ff788599b9e8b8886775ea00373
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2024-01-14 20:44:26 -0500

    Add files via upload

---
+++ src/components/UserInfo.js
@@ -0,0 +1,31 @@
+export default class UserInfo {
+  constructor(profileTitleSelector, profileDescriptionSelector, avatarSelector) {
+    this._profileTitle = document.querySelector(profileTitleSelector);
+    this._profileDescription = document.querySelector(
+      profileDescriptionSelector
+    );
+    this._avatar = document.querySelector(
+      avatarSelector);
+  }
+  // return object containing the text content of the profile elements
+  getUserInfo() {
+    return {
+      title: this._profileTitle.textContent,
+      description: this._profileDescription.textContent,
+      avatar: this._avatar
+    };
+  }
+  //pass in the values from the form submission
+  setUserInfo({ name, about }) {
+    this._profileTitle.textContent = name;
+    this._profileDescription.textContent = about;
+  }
+
+
+setUserAvatar(avatar) {
+  console.log(data)
+  this_avatar.src = avatar;
+};
+
+}; 
+
---
+++ src/pages/index.js
@@ -0,0 +1,205 @@
+// Import all the classes
+import Card from "../components/Card.js";
+import Section from "../components/Section.js";
+import FormValidator from "../components/FormValidator.js";
+import "../pages/index.css";
+import UserInfo from "../components/UserInfo.js";
+import {
+  validationSettings,
+  addCardModal,
+  profileEditModal,
+  cardListEl,
+  profileTitleInput,
+  profileDescriptionInput,
+  profileEditForm,
+  profileEditButton,
+  addNewCardButton,
+  previewImageEl,
+  previewHeadingEl,
+  cardTitleInput,
+  cardUrlInput,
+  previewCardModal,
+  previewModalContainer,
+  profileTitle,
+  profileDescription,
+  initialCards,
+  deleteCardModal,
+  editProfileAvatarModal
+} from "../utils/constants.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Popup from "../components/Popup.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import Api from "../components/api.js";
+import PopupDeleteConfirm from "../components/PopupDeleteConfirm.js";
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "260f3e4b-e160-43cd-8335-e153b3805f81",
+    "Content-Type": "application/json",
+  },
+});
+
+let cardSection;
+
+api
+  .getInitialCards()
+  .then((res) => {
+    cardSection = new Section(
+      {
+        items: res,
+        renderer: (cardData) => {
+          const card = createCard(cardData);
+          cardSection.addItem(card);
+        },
+      }, 
+      ".cards__list"
+    );
+    cardSection.renderItems();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+api
+  .getUserInfo()
+  .then(({ name, about }) => {
+    profileUserInfo.setUserInfo({
+      name,
+      about,
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const addCardPopup = new PopupWithForm(
+  "#add-card-modal",
+  handleAddCardFormSubmit
+);
+
+addCardPopup.setEventListeners();
+const addFormElement = document.forms["card-form"];
+const addFormValidator = new FormValidator(validationSettings, addFormElement);
+addFormValidator.enableValidation();
+
+const avatarPopup = new PopupWithForm(
+  "#avatar-modal",
+  handleAvatarFormSubmit
+);
+avatarPopup .setEventListeners();
+
+const editFormPopup = new PopupWithForm(
+  "#profile-edit-modal",
+  handleProfileEditSubmit
+);
+
+editFormPopup.setEventListeners();
+const profileEditElement = document.forms["profile-form"];
+const editFormValidator = new FormValidator(
+  validationSettings,
+  profileEditElement
+);
+editFormValidator.enableValidation();
+
+const profileUserInfo = new UserInfo(
+  ".profile__title",
+  ".profile__description",
+  ".profile__image"
+);
+
+const popupWithImage = new PopupWithImage("#preview-card-modal");
+popupWithImage.setEventListeners();
+
+// All the rest
+
+function handleImageClick(link, name) {
+  previewImageEl.src = link;
+  previewImageEl.alt = name;
+  previewHeadingEl.textContent = name;
+  popupWithImage.open(link, name);
+}
+
+function createCard(cardData) {
+  const cardElement = new Card(cardData, "#card-template", handleImageClick, handleDeleteButton);
+  return cardElement.getView();
+}
+
+const cardDeleteModal = new PopupDeleteConfirm("#delete-modal");
+
+
+function handleDeleteButton(id) {
+  cardDeleteModal.open();
+  cardDeleteModal.setEventListeners();
+  cardDeleteModal.setSubmitAction(() => {
+    api.
+    deleteCard(id)
+    .then(() => {
+      
+      cardDeleteModal.close();
+      handleDeleteCard(id);
+      
+    })
+    
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+
+}
+
+
+function handleProfileEditSubmit({ title, description }) {
+  editFormPopup.setLoading(true, "Saving ...");
+  api.
+    updateUserInfo({ title, description }).then((res) => {
+    profileUserInfo.setUserInfo(res);
+  });
+  editFormPopup.close();
+}
+
+
+function handleAvatarFormSubmit(inputValues) {
+  
+  api
+    .updateAvatar(inputValues.url)
+    .then((res) => {
+      avatarPopup.setUserAvatar(url);
+      avatarPopup.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+const openAvatarButton = document.querySelector(".profile__icon");
+
+openAvatarButton.addEventListener("click", () => {
+  avatarPopup.open();
+})
+
+function handleAddCardFormSubmit(inputValues) {
+  const cardData = createCard({
+    name: inputValues.title,
+    link: inputValues.url,
+  });
+  api.getNewCard(inputValues).then((res) => cardSection.addItem(cardData));
+  addCardPopup.close().catch((err) => {
+    console.log(err);
+  });
+}
+
+profileEditButton.addEventListener("click", () => {
+  editFormValidator.resetValidation();
+  const userInfo = profileUserInfo.getUserInfo();
+  profileTitleInput.value = userInfo.title;
+  profileDescriptionInput.value = userInfo.description;
+  editFormPopup.open();
+});
+
+;  
+
+addNewCardButton.addEventListener("click", () => {
+  addCardPopup.open();
+  addFormValidator.resetValidation();
+});
