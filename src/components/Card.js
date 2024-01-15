c00361e3fc07b57a71e236ae7dd02db806d57eed
Natalia2311 <138008528+Natalia2311@users.noreply.github.com>
2024-01-15 10:55:37 -0500

    Add files via upload

---
+++ src/components/Card.js
@@ -0,0 +1,79 @@
+class Card {
+  constructor(cardData, cardSelector, handleImageClick, handleDeleteButton, handleCardLike) {
+    this._name = cardData.name;
+    this._link = cardData.link;
+    this._cardSelector = cardSelector;
+    this._handleImageClick = handleImageClick;
+    this.isLiked = cardData.isLiked;
+    this._id = cardData._id;
+    this._handleDeleteButton = handleDeleteButton;
+    this._handleCardLike = handleCardLike;
+  };
+
+  setLikeStatus(isLiked) {
+    this.isLiked = isLiked;
+    this._renderLikes();
+  }
+
+  _renderLikes() {
+    if(!this.isLiked) {
+      this._likeButton.querySelector(".card__like-button"),classList.add("card__like-button_active");
+     } else {
+      this._likeButton.querySelector(".card__like-button"),classList.remove("card__like-button_active");
+    }
+  }
+
+  _setEventListeners() {
+    this._likeButton.addEventListener("click", () => {
+      this._handleCardLike(this);
+    });
+  
+
+    this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteButton(this);
+       
+      });
+
+    this._cardImageEl.addEventListener("click", () => {
+      this._handleImageClick(this._link, this._name, this._id); 
+    });
+  }
+
+  handleDeleteCard() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  // _handleLikeIcon() {
+  //   this._likeButton.classList.toggle("card__like-button_active");
+  // }
+
+  _getElement() {
+    return document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+  }
+
+  getView() {
+    this._cardElement = this._getElement();
+    this._cardImageEl = this._cardElement.querySelector(".card__image");
+    this._cardTitleEl = this._cardElement.querySelector(".card__title");
+    this._likeButton = this._cardElement.querySelector(".card__like-button");
+    this._deleteButton = this._cardElement.querySelector(
+      ".card__delete-button"
+    );
+    this._cardImageEl.src = this._link;
+    this._cardImageEl.alt = this._name;
+    this._cardTitleEl.textContent = this._name;
+   // this._renderLikes();
+    this._setEventListeners();
+    
+
+    return this._cardElement;
+  }
+}
+
+export default Card;
---
+++ src/components/api.js
@@ -0,0 +1,93 @@
+
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponce(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+      })
+      .then((res) => this._checkResponce(res));    
+}
+
+getNewCard({ title, url }) { // name might be different
+  return fetch(`${this._baseUrl}/cards`, {
+    method: "POST",
+    headers: this._headers,
+    body: JSON.stringify({
+    name: title,
+    link: url  
+    }),
+  })
+  .then((res) => this._checkResponce(res));    
+}
+
+getUserInfo() {
+  return fetch(`${this._baseUrl}/users/me`, {
+    method: "GET",
+    headers: this._headers,
+  })
+  .then((res) => this._checkResponce(res));    
+}
+  
+updateUserInfo({ title, description }) {
+  return fetch(`${this._baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: this._headers,
+    body: JSON.stringify({
+      name: title,
+      about: description
+    }),
+    })
+    .then((res) => this._checkResponce(res));   
+}
+
+updateAvatar(url) {
+  return fetch(`${this._baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: this._headers,
+    body: JSON.stringify({
+      avatar: url
+    }),
+    })
+    .then((res) => this._checkResponce(res));   
+}
+
+
+deleteCard(id) {
+  return fetch(`${this._baseUrl}/cards/${id}`, {
+    method: "DELETE",
+    headers: this._headers,
+    })
+    .then((res) => this._checkResponce(res));    
+}
+
+updateLike(id) {
+  return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+    method: "PUT",
+    headers: this._headers,
+    })
+    .then((res) => this._checkResponce(res));    
+}
+
+removeLike(id) {
+  return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+    method: "DELETE",
+    headers: this._headers,
+    })
+    .then((res) => this._checkResponce(res));    
+}
+}
+
+export default Api;
+
